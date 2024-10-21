bde75e66ee54cfd34cbdb344f4d7ae79d6980237
JakubMlocek <jakub.mlocek.informatyka@gmail.com>
2024-10-21 13:49:41 +0200

    third host added

--- demo3hosts.sh
+++ demo3hosts.sh
@@ -5,7 +5,7 @@
 docker-compose down
 docker-compose up --build -d
 
-sleep 10
+sleep 7
 
 echo "Sprawdzanie, czy usługa jest uruchomiona"
 if curl -s http://localhost:1234/blocks > /dev/null; then
@@ -19,15 +19,18 @@ echo "Inicjowanie 3 węzłów w sieci blockchain..."
 curl -X POST http://localhost:1234/init/app1:5000
 curl -X POST http://localhost:1234/init/app2:5000
 curl -X POST http://localhost:1234/init/app3:5000
+echo -e "\n"
 
 #Wyświetlenie aktywnych węzłów
 echo "Wyświetlanie aktywnych węzłów w sieci..."
 curl -X GET http://localhost:1234/nodes
+echo -e "\n"
+
 
 #Przeglądanie blockchaina
 echo "Bloki w łańcuchu widziane przez node1:"
 curl -X GET http://localhost:1234/blocks
-
+echo -e "\n"
 
 #Wykopanie nowego bloku przez node 1
 echo "Kopanie nowego bloku z danymi przez node1:"
@@ -36,6 +39,30 @@ curl -X POST http://localhost:1234/mine -H "Content-Type: application/json" -d '
 #Wykopanie nowego bloku przez node 1
 echo "Kopanie nowego bloku z danymi przez node1:"
 curl -X POST http://localhost:1234/mine -H "Content-Type: application/json" -d '{"data": "Drugi wykopany blok - node1"}'
+echo -e "\n"
+
+
+sleep 3
+
+#Przeglądanie blockchaina
+echo "Bloki w łańcuchu widziane przez node1:"
+curl -X GET http://localhost:1234/blocks
+
+#Przeglądanie blockchaina
+echo "Bloki w łańcuchu widziane przez node2:"
+curl -X GET http://localhost:1235/blocks
+
+#Przeglądanie blockchaina
+echo "Bloki w łańcuchu widziane przez node3:"
+curl -X GET http://localhost:1236/blocks
+echo -e "\n"
+
+
+#Wykopanie nowego bloku przez node 2
+echo "Kopanie nowego bloku z danymi przez node2:"
+curl -X POST http://localhost:1235/mine -H "Content-Type: application/json" -d '{"data": "Trzeci wykopany blok - node2"}'
+
+echo -e "\n"
 
 sleep 3
 
@@ -51,20 +78,6 @@ curl -X GET http://localhost:1235/blocks
 echo "Bloki w łańcuchu widziane przez node3:"
 curl -X GET http://localhost:1236/blocks
 
-#Przeglądanie blockchaina
-echo "Bloki w łańcuchu widziane przez node1 po wykopaniu bloku:"
-curl -X GET http://localhost:1234/blocks
 
-#Przeglądanie blockchaina
-echo "Bloki w łańcuchu widziane przez node2 po wykopaniu bloku:"
-curl -X GET http://localhost:1235/blocks
-
-#Wykopanie nowego bloku przez node 2
-echo "Kopanie nowego bloku z danymi przez node2:"
-curl -X POST http://localhost:1235/mine -H "Content-Type: application/json" -d '{"data": "Trzeci wykopany blok - node2"}'
-
-#Przeglądanie blockchaina
-echo "Bloki w łańcuchu widziane przez node1:"
-curl -X GET http://localhost:1234/blocks
 
 docker-compose down
