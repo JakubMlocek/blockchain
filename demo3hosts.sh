656f5f74cc1c3c09a311d768307e01f44e186ae4
JakubMlocek <jakub.mlocek.informatyka@gmail.com>
2024-10-21 13:33:45 +0200

    third host added

--- demo3hosts.sh
+++ demo3hosts.sh
@@ -8,8 +8,8 @@ else
     echo "Usługa nie działa." 
 fi
 
-#Inicjalizacja 2 wezłow
-echo "Inicjowanie 2 węzłów w sieci blockchain..."
+#Inicjalizacja 3 wezłow
+echo "Inicjowanie 3 węzłów w sieci blockchain..."
 curl -X POST http://localhost:1234/init/app1:5000
 curl -X POST http://localhost:1234/init/app2:5000
 curl -X POST http://localhost:1234/init/app3:5000
@@ -23,19 +23,17 @@ curl -X GET http://localhost:1234/nodes
 echo "Bloki w łańcuchu widziane przez node1:"
 curl -X GET http://localhost:1234/blocks
 
-#Dodanie bloku z innego węzła 
-echo "Symulacja dodania bloku z innego węzła..."
-curl -X POST http://localhost:1234/store_data -H "Content-Type: application/json" -d '{
-    "data": "Dane z innego wezla",
-    "prev_hash": "0000000000000000000000000000000000000000000000000000000000000000",
-    "hash": "b5d4045c49a1e50ef6b2f83c8e07a3b9932cc705f3e7d46abf738effa1c4d1d1",
-    "nonce": 283
-}'
-
 #Przeglądanie blockchaina
 echo "Bloki w łańcuchu widziane przez node1:"
 curl -X GET http://localhost:1234/blocks
 
+#Przeglądanie blockchaina
+echo "Bloki w łańcuchu widziane przez node2:"
+curl -X GET http://localhost:1235/blocks
+
+#Przeglądanie blockchaina
+echo "Bloki w łańcuchu widziane przez node3:"
+curl -X GET http://localhost:1236/blocks
 
 #Wykopanie nowego bloku przez node 1
 echo "Kopanie nowego bloku z danymi przez node1:"
